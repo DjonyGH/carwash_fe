f631ac4d33fcf4b245cb91d9ae83457be38af6f9
Evgeniy Kalimullin <counsil1986@gmail.com>
2021-10-09 11:48:34 +0700

    created router, mainPage, notFoundPage

---
+++ src/pages/main/MainPage.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from 'react'
+
+const MainPage: FC = () => {
+  return <div>Главная страница</div>
+}
+
+export default MainPage
---
+++ src/router/index.ts
@@ -0,0 +1,30 @@
+import React from 'react'
+import MainPage from '../pages/main/MainPage'
+import NoFoundPage from '../pages/notFound/NotFoundPage'
+
+export interface IRoute {
+  path: string
+  component: React.ComponentType
+  exact?: boolean
+}
+export enum ERoutes {
+  HEAD = '/',
+  ORDER = '/order',
+  PROFILE = '/profile',
+  ABOUT_US = '/about_us',
+  NOT_FOUND = '/not-found',
+}
+
+export const publicRoutes: IRoute[] = [
+  {
+    path: ERoutes.HEAD,
+    component: MainPage,
+    exact: true,
+  },
+  {
+    path: ERoutes.NOT_FOUND,
+    component: NoFoundPage,
+  },
+]
+
+export const privatRoutes: IRoute[] = []
